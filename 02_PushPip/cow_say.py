1e67cec29f5fdd0a2050ddb6aa246cfa69f2e037
temnoosh <soryualliance@gmail.com>
2023-03-01 21:14:09 +0300

    add eyes argumet

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,7 +3,14 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("message")
+parser.add_argument('-e', '--eyes')
+parser.add_argument('-f', '--cowfile')
+#[-e eye_string] [-f cowfile] [-h] [-l] [-n] [-T tongue_string] [-W column]
 args = vars(parser.parse_args())
 msg = args["message"]
 
-print(cowsay.cow(msg))
+del args["message"]
+if args["eyes"] is None:
+    del args["eyes"]
+
+print(cowsay.cowsay(msg, **args))
