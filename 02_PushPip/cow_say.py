f7321b6d62e473276d95bc843d5ec10480ad185b
temnoosh <soryualliance@gmail.com>
2023-03-01 21:51:49 +0300

    add width and tongue argument

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -4,7 +4,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("message")
 parser.add_argument('-e', '--eyes')
-parser.add_argument('-l', '--list', action='store_true')
+parser.add_argument('-T', '--tongue')
+parser.add_argument('-W', '--width', default=40, type=int)
+parser.add_argument('-l', '--list', action='store_true', default=False)
 parser.add_argument('-f', '--cowfile')
 #[-e eye_string] [-f cowfile] [-h] [-l] [-n] [-T tongue_string] [-W column]
 args = vars(parser.parse_args())
@@ -13,8 +15,10 @@ msg = args["message"]
 del args["message"]
 if args["eyes"] is None:
     del args["eyes"]
+if args["tongue"] is None:
+    del args["tongue"]
 
-if args["list"] is None:
+if not args["list"]:
     del args["list"]
     print(cowsay.cowsay(msg, **args))
 else:
